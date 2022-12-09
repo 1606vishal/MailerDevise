ac9a5dd6d41c4d5a9356e82366f78b32a644d577
1606vishal <vishaltale417@gmail.com>
2022-12-09 15:10:46 +0530

    Finall commit

---
+++ config/environment.rb
@@ -0,0 +1,20 @@
+# Load the Rails application.
+require_relative "application"
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            '<username>',
+  password:             '<password>',
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
+
+
+
